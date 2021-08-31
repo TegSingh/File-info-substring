815e758e73663a86672f58a5f4bfea49017055e1
TegSingh <tegveer2211@gmail.com>
2021-08-30 21:01:41 -0400

    runs correctly uptill 5000 with initial bucket name

--- script.py
+++ script.py
@@ -1,9 +1,17 @@
 import os
 import sys
 import csv
+import timeit
+
+# Set the bucket name for all the file names to compare against
+bucket_name = "com.ibm.ws.microprofile.config.1.1_fat"
 
 def main():
     file_substring()
+    # Test the find_common_substring method
+    string1 = "com.ibm.ws"
+    string2 = "com.ibm.ws.microprofile.config"
+    print(find_common_substring(string1, string2))
 
 # Starter method to add another column showing whether a match occured in the csv file
 def file_substring(): 
@@ -13,26 +21,65 @@ def file_substring():
             csv_writer = csv.writer(file_writer)
             line_count = 0
             for row in csv_reader:
-                if line_count == 0:
-                    # Add the column header to the file
-                    row.append('Match')
-                    csv_writer.writerow(row)
-                    line_count += 1
-                else: 
-                    # Check whether a matching file substring exists
-                    bucket_name = 'Hello'
-                    file_name = row[0]
-                    match = find_substring_match(bucket_name, file_name)
-                    row.append(match)
-                    csv_writer.writerow(row)
-                    line_count += 1
+                print(row)
+                if line_count < 5000: 
+                    if line_count == 0:
+                        # Add the column header to the file
+                        row.append('Match')
+                        csv_writer.writerow(row)
+                        line_count += 1
+                    else: 
+                        # Check whether a matching file substring exists
+                        file_name = row[0]
+                        match = find_substring_match(bucket_name, file_name)
+                        row.append(match)
+                        csv_writer.writerow(row)
+                        line_count += 1
+                else:
+                    break
     
 # Method to find match with the substring and files
 def find_substring_match(bucket_name, file_name):
-    print(bucket_name, file_name)
-    match_found = False
+    
+    # Intialize the match value to false
+    match = False
+    
+    # Split string based on /
+    split_string = file_name.split('/')
+    
+    for i in range(len(split_string)):
+        # Give priority to the files that are in dev directory
+        if split_string[0] == 'dev':
+            match, matchstring = find_common_substring(bucket_name, split_string[1])
+    
+    return match
 
-    return match_found
+# Method to find the longest common substring between 2 substrings (starting at index 0) passed as arguments
+def find_common_substring(string1, string2):
+    l1 = len(string1)
+    l2 = len(string2)
+    j = 0    
+    common = ""
+    for i in range(l1):
+        if i == j:
+            if string1[i] == string2[j]:
+                j += 1
+                # Add the matching string to the value for common
+                common += string1[i]
+            else:
+                break
+        else: 
+            break   
+    
+    # If the length of bucket name and file name end up being a perfect match
+    if len(common) == len(string1) or len(common) == len(string2):
+        return True, common
+    # If common ends with ., only then return true
+    elif common != "" and common[len(common) - 1] == '.':
+        return True, common
+    else:
+        return False, "" 
+    
 
 if __name__ == '__main__':
     main()
